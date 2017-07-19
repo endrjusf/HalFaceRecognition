f070197c1a6556fd9905d3fca5a558362aac7f97
Andrzej Ficoń <andrzej.ficon@makingwaves.com>
2017-07-19 19:48:41 +0200

    Initial commit

---
+++ Hal/FaceAdder/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Face;
+using Microsoft.ProjectOxford.Face.Contract;
+
+namespace FaceAdder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            FaceServiceClient faceServiceClient = new FaceServiceClient("01e1692026654958a58bf31618195b51", "https://westcentralus.api.cognitive.microsoft.com/face/v1.0");
+
+
+            Task.Run(async () =>
+            {
+                GetValue(faceServiceClient);
+            }).GetAwaiter().GetResult();
+            Console.ReadKey();
+        }
+
+        private static async Task WaitCallLimitPerSecondAsync(Queue<DateTime> _timeStampQueue, int CallLimitPerMinute)
+        {
+            async Task WaitCallLimitPerSecondAsync()
+            {
+                Monitor.Enter(_timeStampQueue);
+                try
+                {
+                    if (_timeStampQueue.Count >= CallLimitPerMinute)
+                    {
+                        TimeSpan timeInterval = DateTime.UtcNow - _timeStampQueue.Peek();
+                        if (timeInterval < TimeSpan.FromMinutes(1))
+                        {
+                            await Task.Delay(TimeSpan.FromMinutes(1) - timeInterval);
+                        }
+                        _timeStampQueue.Dequeue();
+                    }
+                    _timeStampQueue.Enqueue(DateTime.UtcNow);
+                }
+                finally
+                {
+                    Monitor.Exit(_timeStampQueue);
+                }
+            }
+        }
+
+        private static async Task GetValue(FaceServiceClient faceServiceClient)
+        {
+            try
+            {
+                const int PersonCount = 10000;
+                const int CallLimitPerSecond = 20;
+                Queue<DateTime> _timeStampQueue = new Queue<DateTime>(CallLimitPerSecond);
+                _timeStampQueue.Enqueue(DateTime.UtcNow);
+
+                const string personGroupId = "hal_group1";
+                const string personGroupName = "HalGroup1";
+
+                await WaitCallLimitPerSecondAsync(_timeStampQueue, CallLimitPerSecond);
+
+                //var group = await faceServiceClient.GetPersonGroupAsync(personGroupId);
+                //if (group != null)
+                //{
+                //    await faceServiceClient.DeletePersonGroupAsync(personGroupId);
+                //}
+                //await faceServiceClient.CreatePersonGroupAsync(personGroupId, personGroupName);
+
+                string personImageDir = @"C:\HalPhotos";
+                var items = Directory.GetFiles(personImageDir);
+
+                CreatePersonResult[] persons = new CreatePersonResult[items.Length];
+                var i = 0;
+                foreach (string path in items)
+                {
+                    Thread.Sleep(10000);
+                    string personName = path.Split(new[] {'\\'}).Last();
+                    persons[i] = await faceServiceClient.CreatePersonAsync(personGroupId, personName);
+                    Guid personId = persons[i].PersonId;
+                    //foreach (string imagePath in Directory.GetFiles(path, "*.jpg"))
+                    {
+                        await WaitCallLimitPerSecondAsync(_timeStampQueue, CallLimitPerSecond);
+                        Console.WriteLine(personName + " " + personName);
+                        using (Stream stream = File.OpenRead(path))
+                        {
+                            await faceServiceClient.AddPersonFaceAsync(personGroupId, personId, stream);
+                        }
+                    }
+
+                    i++;
+                }
+
+                await faceServiceClient.TrainPersonGroupAsync("hal_group1");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+        
+        
+    }
+}
---
+++ Hal/Hal/MainPage.xaml.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Windows.Foundation;
+using Windows.Media.Capture;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Storage.Pickers;
+using Microsoft.ProjectOxford.Face;
+
+//using Microsoft.ProjectOxford.Face;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Hal
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        //MainPage rootPage = MainPage.Current;
+        private Windows.Foundation.Collections.IPropertySet appSettings;
+        private const String photoKey = "capturedPhoto";
+        private FaceServiceClient faceServiceClient;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            appSettings = ApplicationData.Current.LocalSettings.Values;
+            this.faceServiceClient = new FaceServiceClient("01e1692026654958a58bf31618195b51", "https://westcentralus.api.cognitive.microsoft.com/face/v1.0");
+
+            //ResetButton.Visibility = Visibility.Collapsed;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //var captureManager = new MediaCapture();
+                //await captureManager.InitializeAsync();
+                //var imgFormat = ImageEncodingProperties.CreatePng();
+
+                //// create storage file in local app storage
+                //var file = await ApplicationData.Current.LocalFolder.CreateFileAsync(
+                //    "face.png",
+                //    CreationCollisionOption.ReplaceExisting);
+                //// take photo
+                //await captureManager.CapturePhotoToStorageFileAsync(imgFormat, file);
+                //rootPage.NotifyUser("", NotifyType.StatusMessage);
+
+                //Using Windows.Media.Capture.CameraCaptureUI API to capture a photo
+                CameraCaptureUI dialog = new CameraCaptureUI();
+                Size aspectRatio = new Size(16, 9);
+                dialog.PhotoSettings.CroppedAspectRatio = aspectRatio;
+
+                var uploadedFile = this.File.Text;
+
+                //if (string.IsNullOrWhiteSpace(uploadedFile))
+                {
+
+                    var filePicker = new FileOpenPicker();
+                    filePicker.FileTypeFilter.Add("*");
+                    //StorageFile file = await filePicker.PickSingleFileAsync();
+
+                    //if (file != null)
+                    //{
+                        // BitmapImage bitmapImage = new BitmapImage();
+                       
+
+                    StorageFile file = await dialog.CaptureFileAsync(CameraCaptureUIMode.Photo);
+                    if (file != null)
+                    {
+                        using (var fileStream = await file.OpenAsync(FileAccessMode.Read))
+                        {
+                            //var people = await this.faceServiceClient.ListPersonsAsync("hal_group1");
+                           
+                            var image = fileStream.AsStream();
+
+                            var id = await this.faceServiceClient.DetectAsync(image);
+
+                            if (id.Length == 0)
+                            {
+                                this.Result.Text = "USER UNKNOWN";
+                                return;
+                            }
+
+                            //await this.faceServiceClient.CreateFaceListAsync("test", "test");
+                            //var face = await this.faceServiceClient.AddFaceToFaceListAsync("test", image);
+                            //this.Result.Text = face.PersistedFaceId.ToString();
+                            var result = await this.faceServiceClient.IdentifyAsync("hal_group1", new [] { id[0].FaceId }, 0.0001f, 2);
+
+
+                            if (result.Length == 0)
+                            {
+                                this.Result.Text = "USER UNKNOWN";
+                            }
+                            else
+                            {
+                                var sb = new StringBuilder();
+
+                                foreach (var r in result)
+                                {
+                                    sb.Append("Face ID: ");
+                                    sb.Append(r.FaceId);
+                                    sb.Append(" candidates: ");
+
+                                    foreach (var c in r.Candidates)
+                                    {
+                                        var person = await this.faceServiceClient.GetPersonAsync("hal_group1", c.PersonId);
+                                        sb.Append($"{person.Name} {c.Confidence}, ");
+                                    }
+                                }
+
+                                //var id = result.First().FaceId;
+                                //var person = await this.faceServiceClient.GetPersonAsync("hal_group1", id);
+                                this.Result.Text = sb.ToString();
+                            }
+                        }
+
+                    }
+                }
+                //else
+                //{
+                //    var filePicker = new FileOpenPicker();
+                //    filePicker.FileTypeFilter.Add("*");
+                //    StorageFile file = await filePicker.PickSingleFileAsync();
+
+                //    if (file != null)
+                //    {
+                //        // BitmapImage bitmapImage = new BitmapImage();
+                //        using (var fileStream = await file.OpenAsync(FileAccessMode.Read))
+                //        {
+                //            var image = fileStream.AsStream();
+                //            var result = await this.faceServiceClient.DetectAsync(image, true, false, null);
+                //            this.Result.Text = (result.Length == 0) ? "USER UNKNOWN" : result[0].FaceId.ToString();
+                //        }
+
+                //    }
+
+                 
+
+                //}
+            }
+            catch (Exception ex)
+            {
+                this.Result.Text = ex.ToString();
+            }
+
+
+        }
+    }
+}
